542a188f335205c497e07d32deb4ebfac2f6dd03
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-10 15:15:09 +0900

    Set panic hook

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,11 +1,12 @@
 mod renderer;
 
-use log::info;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_console_logger::DEFAULT_LOGGER;
 
 #[wasm_bindgen]
 pub fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     log::set_logger(&DEFAULT_LOGGER).unwrap();
     log::set_max_level(log::LevelFilter::Info);
 }
